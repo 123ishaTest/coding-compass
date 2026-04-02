1a37aceaeaf5e443f2294d4f7f84553b3d30f484
Isha Dijcks <ishadijcks@gmail.com>
2026-04-02 22:39:39 +0200

    No longer hide menu on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 <div class="flex h-screen flex-col">
   <div class="navbar bg-base-100 shadow-sm">
     <div class="navbar-start"></div>
-    <div class="navbar-center hidden lg:flex">
+    <div class="navbar-center flex">
       <ul class="menu menu-horizontal px-1">
         <li><a href={resolve('/')}>Home</a></li>
 
